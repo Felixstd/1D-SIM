91600e8ce75264ee3d1e6f0d2eb6c50d0746c3c4
Felixstd <felix.st-denis@umontreal.ca>
2026-04-30 10:02:07 -0400

    run_1dsim: update with namelist
    Updated the model script to run with the namelist and input file.

--- run_1dsim.sh
+++ run_1dsim.sh
@@ -6,22 +6,24 @@ set -e
 # Define variables
 EXPNO="19"
 TARGET_DIR="Experiments/$EXPNO"
-SOURCE_FILE="./src/ice.f90"   # Change this to your source file name
+SOURCE_FILE="namelistSIM"   # Change this to your source file name
 EXECUTABLE="zoupa"
-POST_FILE="./output_post_files/output_mu_test_$EXPNO"
+POST_FILE="./output_post_files/output_wp_$EXPNO"
 
-# Step 1: Create the directory if it doesn't exist
-mkdir -p "$TARGET_DIR"
+# Step 0: Clean the previous build
+make clean
 
-# Step 2: Copy the source file to the directory
-cp "$SOURCE_FILE" "$TARGET_DIR/"
-
-
-# Step 4: Compile the model (Fortran example using gfortran)
+# Step 1: Compile the model
 make
 
-# Step 5: Run the model
-nohup ./"$EXECUTABLE" > "$POST_FILE" &
+# Step 2: Create the directory if it doesn't exist
+mkdir -p "$TARGET_DIR"
+
+# Step 3: Copy the source file to the directory
+cp "$SOURCE_FILE" "$TARGET_DIR/"
+
+# Step 4: Run the model
+nohup ./"$EXECUTABLE" < input_1dsim > "$POST_FILE" &
 
 wait &
 
