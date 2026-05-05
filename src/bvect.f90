44a68faca4bfcc25be6114a66591e44f5b6a685c
Felixstd <felix.st-denis@umontreal.ca>
2026-05-05 10:44:34 -0400

    bvect: new P regularization
    **The code does not work right now because not
    all necessary solver related subroutines have
    been updated.**
    
    Added the necessary calls to add the new laplacian
    regularization in the stress tensor. Since this new
    term does not depend on the velocity, I added it to
    the formulation of the b vector when solving
    
    A(u)*u = b
    
    - `bvect.f90` updated.
    
    Gitlab Issue 0004

--- src/bvect.f90
+++ src/bvect.f90
@@ -5,13 +5,21 @@ subroutine bvect(tauair, un1, Cw, b)
   use numerical
   use global_var
   use shallow_water
+  use option
+  use rheology
 
   implicit none
       
   integer :: i
   double precision h_at_u, a_at_u
+  double precision :: lap_P_half(1:nx+1)
   double precision, intent(in) :: tauair(1:nx+1), un1(1:nx+1), Cw(1:nx+1)
   double precision, intent(out):: b(1:nx+1)
+
+
+  if (nonlocal) then 
+    call laplacian(2d0*P_half, lap_P_half)
+  endif
 
   b(1)    = 0d0 ! close bc
   b(nx+1) = 0d0 ! close bc 
@@ -25,7 +33,10 @@ subroutine bvect(tauair, un1, Cw, b)
      b(i) = a_at_u*tauair(i) + a_at_u*Cw(i)*uwn2(i) - &
             ( P_half(i) - P_half(i-1) ) / Deltax + ( rho * h_at_u * un1(i) ) / Deltat - &
             rho * h_at_u * ge * ( etawn1(i) - etawn1(i-1) ) / Deltax
-  
+
+    if (nonlocal) then 
+        b(i) = b(i) + l_scale2*( lap_P_half(i) - lap_P_half(i-1) ) / Deltax
+    endif
 
   enddo
    
