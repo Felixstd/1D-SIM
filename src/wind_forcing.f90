f6a1e013775bc8be65be2687c6fa7ac49b48a427
Felixstd <felix.st-denis@umontreal.ca>
2026-05-07 11:59:35 -0400

    wind_forcing: aesthetics and comments
    Removed the commented `tauair` in the constant_wind
    section. For tauair to be zero, just set uwind to be 0.
    
    Also changed the tabulation to be 4 spaces and uniform
    throughout the subroutine.

--- src/wind_forcing.f90
+++ src/wind_forcing.f90
@@ -2,55 +2,53 @@
 
 subroutine wind_forcing (tauair, ts)
 
-  use size
-  use forcing
-  use resolution
-  use option
+   use size
+   use forcing
+   use resolution
+   use option
 
-  implicit none
+   implicit none
      
-  integer :: i
-  integer, intent(in) :: ts
-  double precision :: period, modulation, pi, apar
-  double precision, intent(out) :: tauair(1:nx+1) ! air drag
-  double precision :: Tramp, rampfactor, vel_wind
+   integer :: i
+   integer, intent(in) :: ts
+   double precision :: period, modulation, pi, apar
+   double precision, intent(out) :: tauair(1:nx+1) ! air drag
+   double precision :: Tramp, rampfactor, vel_wind
 
 
-  period = 6d0*3600d0 ! period of cos in seconds (set to 3 days)
-  pi = 3.14159265d0
-  apar = 6d0*3600d0
+   period = 6d0*3600d0 ! period of cos in seconds (set to 3 days)
+   pi = 3.14159265d0
+   apar = 6d0*3600d0
 
-  tauair(1)    = 0d0 ! close bc
-  tauair(nx+1) = 0d0 ! close bc
+   tauair(1)    = 0d0 ! close bc
+   tauair(nx+1) = 0d0 ! close bc
 
-    if (constant_wind) then
+   if (constant_wind) then
 
-       do i = 2, nx ! with apar = 6*3600, tauair is (1-e^-2) after 12 hours.
-          tauair(i) = Cda * abs(uwind)*uwind
-         !  tauair(i) = 0d0
-!	tauair(i) = (Cda * abs(uwind)*uwind)*(1d0-exp(-1d0*ts*Deltat/apar)) ! at n       
-       enddo
+      do i = 2, nx ! with apar = 6*3600, tauair is (1-e^-2) after 12 hours.
+         tauair(i) = Cda * abs(uwind)*uwind
+      enddo
 
       
-      elseif (rampupwind) then
+   elseif (rampupwind) then
 
-         Tramp = 60*60*2
-         rampfactor = tanh(ts*Deltat/Tramp)
-         vel_wind = uwind*rampfactor
-         do i = 2, nx
-          tauair(i) = Cda * (vel_wind)**2d0
-         enddo
+      Tramp = 60*60*2
+      rampfactor = tanh(ts*Deltat/Tramp)
+      vel_wind = uwind*rampfactor
+      do i = 2, nx
+         tauair(i) = Cda * (vel_wind)**2d0
+      enddo
 
-      else
-	   modulation = sin(2*pi*ts*Deltat/period)
-       
-       do i = 2, nx
-          tauair(i) = modulation * Cda * (uwind)**2d0
-       enddo
+   else
+      modulation = sin(2*pi*ts*Deltat/period)
 
-    endif
+      do i = 2, nx
+         tauair(i) = modulation * Cda * (uwind)**2d0
+      enddo
 
-  return
+   endif 
+
+   return
 end subroutine wind_forcing
 
 
