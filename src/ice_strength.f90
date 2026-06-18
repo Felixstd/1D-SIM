43979d7c35868d2c28adc56a0a237fbfd49538e4
Felixstd <felix.st-denis@umontreal.ca>
2026-06-18 11:23:22 -0400

    ice_strength: linear in A and h
    Added the new option for Pp. The ice strength can now
    be computed as
    
    P = P*hA
    
    This is to look at the sensitivity of the fracture with our
    smoothed initial conditions.
    
    - ice_strength.f90 updated.

--- src/ice_strength.f90
+++ src/ice_strength.f90
@@ -19,11 +19,19 @@ subroutine ice_strength ( hin, Ain, uin)
   Tp_half(0)    = 0d0 ! ! sea ice pressure / 2d0
   Tp_half(nx+1) = 0d0
 
-  do i = 1, nx
-    Pp_half(i) = 0.5d0 * Pstar_prime(i) * hin(i) * dexp(-C * ( 1d0 - Ain(i) ) )
-    Tp_half(i) = kt*Pp_half(i)
-  enddo
-  
+
+  if (pp_linear) then 
+    do i = 1, nx
+      Pp_half(i) = 0.5d0 * Pstar * hin(i) * Ain(i) 
+      Tp_half(i) = kt*Pp_half(i)
+    enddo
+  else
+
+    do i = 1, nx
+      Pp_half(i) = 0.5d0 * Pstar * hin(i) * dexp(-C * ( 1d0 - Ain(i) ) )
+      Tp_half(i) = kt*Pp_half(i)
+    enddo
+  endif
 
 !------- set p = 0 at open boundaries for proper care of open bc --------------
 !                    see p.1241-1242 for details              
