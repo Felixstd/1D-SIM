f17a0a8af04b9e0ef97c206489301a2bd1002816
Felixstd <felix.st-denis@umontreal.ca>
2026-06-18 11:06:06 -0400

    ice_strength: removed the Pstar change option
    Removing the Pstar change in this commit since
    it is not being used in this study.
    
    It might come back later, but I just want to make the code clearer.
    
    - ice_strength.f90 updated.

--- src/ice_strength.f90
+++ src/ice_strength.f90
@@ -11,36 +11,7 @@ subroutine ice_strength ( hin, Ain, uin)
 
   integer :: i
   
-  double precision, intent(in) :: hin(0:nx+1), Ain(0:nx+1), uin(0:nx+1)
-  double precision :: deno, P0, rho_prime, dudx
-  double precision :: Pstar_prime(1:nx+1)
-  
-  rho_prime = ((rhowater - rho)/rhowater)*rho
-
-
-  if (Pstart_change) then 
-    do i = 1, nx ! for tracer points
-
-			dudx = ( uin(i+1) - uin(i) ) / Deltax
-
-      ! deno = alpha*sqrt( (dudx)**2d0 ) ! small2 is there to avoid div by zero
-      deno = alpha2*(dudx)**2d0
-
-      if (P0_constant) then 
-        P0 = rho_prime*ge*20d0
-      else
-        P0 = rho_prime*ge*hin(i)
-      endif
-
-      Pstar_prime(i) = P0 + (Pstar - P0)*tanh(deno/sqrt(deno+denomin_P**2d0))
-      ! Pstar_prime(i) = P0 + (Pstar - P0)*tanh(deno/denomin_P)
-    enddo
-  
-  else  
-    do i = 1, nx
-      Pstar_prime(i) = Pstar
-    enddo
-  endif
+  double precision, intent(in) :: hin(0:nx+1), Ain(0:nx+1), uin(0:nx+1)  
 
 
   Pp_half(0)    = 0d0 ! ! sea ice pressure / 2d0
