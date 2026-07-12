e8576e5f6aa0cd4db813cfdcebe8066ec640322d
Felixstd <felix.st-denis@umontreal.ca>
2026-07-11 21:17:28 -0400

    Growth rates: presentation and paper figures
    Finally splitted the code to have different figures depending
    on which I am doing a presentation or paper.
    
    The code can be ran with --mode presentation or paper.
    
    Two different functions will then be run depending on the mode.

--- StabilityAnalysis/GrowthRates_E_n_VP.py
+++ StabilityAnalysis/GrowthRates_E_n_VP.py
@@ -63,6 +63,157 @@ def every_two_symlog(x, pos):
         return r'${}10^{{{}}}$'.format(sign, exp)
     return ''
 
+def plot_paper(k, l_nl, 
+               omega_vp_div, omega_sVP_viscous, 
+               omega_EVP_viscous, omega_EVP_div, 
+               omega_EVP_conv):
+    fig, (ax1, ax2, ax3) = plt.subplots(
+            3, 1,
+            figsize=(4.7,8.7),
+            sharex = True,
+            constrained_layout = True
+        )
+
+    axs = [ax1,ax2,ax3]
+
+    plt.rcParams.update({
+                "font.size": 10,
+                "axes.titlesize": 10,
+                "axes.labelsize": 10,
+                "legend.fontsize": 10,
+                "xtick.labelsize": 8,
+                "ytick.labelsize": 8,
+            })
+
+    ax2.plot(k, omega_vp_div, color="#999999", lw=1.5, alpha = 0.8,label='VP')
+    ax1.plot(k, omega_sVP_viscous, color='#999999', lw=1.5, label='VP', alpha = 0.8)
+    ax1.plot(k, omega_EVP_viscous, color = '#4d4d1a', label = 'EVP')
+    ax1.axvspan(1e-8, np.sqrt(4*rho*Pstarstar*h0**2*(1+C_s*A0)/nu_max**2), alpha=0.3, color='darkgreen')
+    ax1.axvspan(np.sqrt(4*rho*Pstarstar*h0**2*(1+C_s*A0)/nu_max**2), 1,alpha=0.3, color='crimson')
+    for i, l in enumerate(l_nl):
+        ax2.plot(k, VP_nonlocal(k, l)[0], color=okabe_ito[i], lw=1.2,label=r'$l_c = {:.0f}\,\mathrm{{km}}$'.format(l/1e3))
+        # ax1.plot(k, VP_nonlocal_viscous(k, l)[0], color=original_cmap(norm(l)), lw=1.2,label=r'$l_c = {:.0f}\,\mathrm{{km}}$'.format(l/1e3))
+        ax1.plot(k, VP_nonlocal_viscous(k, l)[0], color=okabe_ito[i], lw=1.2,label=r'$l_c = {:.0f}\,\mathrm{{km}}$'.format(l/1e3))
+        ax3.plot(k, VP_nonlocal(k, l)[1], color=okabe_ito[i], lw=1.2,label=r'$l_c = {:.0f}\,\mathrm{{km}}$'.format(l/1e3))
+    linestyles = ['-', '-.', '--']
+
+    count = 0
+    for omega, ls in zip(omega_EVP_div, linestyles):
+        if count == 0:
+            ax2.plot(k, omega, color='#4d4d1a', linestyle=ls, label = 'EVP')
+        else:
+            ax2.plot(k, omega, color='#4d4d1a', linestyle=ls)
+            
+        count+=1
+    for omega, ls in zip(omega_EVP_conv, linestyles):
+        ax3.plot(k, omega, color='#4d4d1a', linestyle=ls)
+
+    for ax in axs:
+        ax.grid(True, alpha=0.5, lw=0.5)
+        ax.tick_params(length=3, width=0.8)
+        
+    labels = ['(a)','(b)','(c)']
+    for i, ax in enumerate(axs):
+            ax.grid(alpha = 0.5)
+            ax.text(0.039, 0.955, labels[i], transform=ax.transAxes,
+                fontsize=10,
+                verticalalignment='top', horizontalalignment='left')
+
+    # fig.canvas.draw()  # force layout computation
+    ax_pos = ax2.get_position()
+    center = (ax_pos.x0 + ax_pos.x1) / 2
+    fig.supxlabel(r'$k$ (m$^{-1}$)', x=center-0.015)
+    # fig.supxlabel(r'$k$ (m$^{-1}$)',x = 0.465)
+    ax1.set_ylabel(r'$\text{Re}\{\omega_+\}$ (s$^{-1}$)')
+    ax2.set_ylabel(r'$\text{Re}\{\omega\}$ (s$^{-1}$)')
+    ax3.set_ylabel(r'$\text{Re}\{\omega\}$ (s$^{-1}$)')
+    # ax2.legend(fontsize=8, frameon=False)
+    ax2.set_xscale('log')
+    ax2.set_yscale('log')
+    ax1.set_xscale('log')
+    ax1.set_title('Viscous')
+    ax2.set_title('Plastic-Divergence')
+    ax3.set_title('Plastic-Convergence')
+    ax3.set_yscale('symlog', linthresh = 1e-3)
+    fig.align_ylabels()
+
+    leg = ax2.legend(
+        loc='center left',
+        bbox_to_anchor=(1.01, 0.5),   # just outside ax1
+        frameon=False,
+        handlelength=0,
+        handletextpad=0
+    )
+    for handle, text in zip(leg.legend_handles, leg.get_texts()):
+        text.set_color(handle.get_color())
+
+    fig.set_constrained_layout_pads(hspace=0.08)
+    fig.savefig("dispersion_VP_nonlocal_a005.png", bbox_inches="tight")
+
+def plot_presentation(k, l_nl, 
+               omega_vp_div, omega_sVP_viscous, 
+               omega_EVP_viscous, omega_EVP_div, 
+               omega_EVP_conv):
+    
+    plt.rcParams.update({
+                "font.size": 12,
+                "axes.titlesize": 12,
+                "axes.labelsize": 12,
+                "legend.fontsize": 12,
+                "xtick.labelsize": 10,
+                "ytick.labelsize": 10,
+            })
+    
+    #-----------------------------------------------
+    # viscous regime 
+    #-----------------------------------------------
+    
+    fig1 = plt.figure(1)
+    
+    
+    
+    
+    plt.close(1)
+    
+    #-----------------------------------------------
+    # Plastic regime 
+    #-----------------------------------------------
+    
+    fig2 = plt.figure(2, figsize = (6,3))
+    ax2 = plt.axes()
+    ax2.spines['top'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+    ax2.plot(k, omega_vp_div, color="#777777", lw=1.5, alpha = 0.8,label='VP')
+    for i, l in enumerate(l_nl):
+        ax2.plot(k, VP_nonlocal(k, l)[0], color=okabe_ito[i], lw=1.2,label=r'$l_c = {:.0f}\,\mathrm{{km}}$'.format(l/1e3))
+    ax2.set_xscale('log')
+    ax2.set_yscale('log')
+    # ax2.spines['bottom'].set_alpha(0.7)
+    # ax2.spines['left'].set_alpha(0.7)
+    # ax2.tick_params(axis='both', alpha = 0.7)
+    leg = ax2.legend(
+        loc='upper left',
+        bbox_to_anchor=(0.01, 1),   # just outside ax1
+        frameon=False,
+        handlelength=0,
+        handletextpad=0
+    )
+    for handle, text in zip(leg.legend_handles, leg.get_texts()):
+        text.set_color(handle.get_color())
+        
+    ax2.set_xlabel(r'$k$ (m$^{-1}$)')
+    
+        
+    ax2.set_ylabel('Re$\{\omega_+\}$\n(s$^{-1}$)', rotation = 0, 
+                   ha = 'left')
+    ax2.yaxis.set_label_coords(-0.22, 0.8) 
+    fig2.savefig('growth_plastic_presentation.png')
+    plt.close(2)
+    
+    
+
+    
+
 
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -81,15 +232,7 @@ def main(argv=None):
     parser = build_parser()
     args = parser.parse_args(argv)
  
-    # if args.list or args.figure is None:
-    #     print("Available styles:")
-    #     for name in styles:
-    #         print(f"  - {name}")
-    #     if args.figure is None and not args.list:
-    #         parser.print_usage()
-    #         return 1
-    #     return 0
-    print(args.mode)
+
     
     # k = np.linspace(1e-8,1, 10000000)
     k = np.logspace(-8, 0, 3000) 
@@ -108,102 +251,16 @@ def main(argv=None):
     
     
     if args.mode == 'paper':
-        fig, (ax1, ax2, ax3) = plt.subplots(
-            3, 1,
-            figsize=(4.7,8.7),
-            sharex = True,
-            constrained_layout = True
-        )
-
-        axs = [ax1,ax2,ax3]
-
-        plt.rcParams.update({
-                    "font.size": 10,
-                    "axes.titlesize": 10,
-                    "axes.labelsize": 10,
-                    "legend.fontsize": 10,
-                    "xtick.labelsize": 8,
-                    "ytick.labelsize": 8,
-                })
-
-        ax2.plot(k, omega_vp_div, color="#999999", lw=1.5, alpha = 0.8,label='VP')
-        ax1.plot(k, omega_sVP_viscous, color='#999999', lw=1.5, label='VP', alpha = 0.8)
-        ax1.plot(k, omega_EVP_viscous, color = '#4d4d1a', label = 'EVP')
-        ax1.axvspan(1e-8, np.sqrt(4*rho*Pstarstar*h0**2*(1+C_s*A0)/nu_max**2), alpha=0.3, color='darkgreen')
-        ax1.axvspan(np.sqrt(4*rho*Pstarstar*h0**2*(1+C_s*A0)/nu_max**2), 1,alpha=0.3, color='crimson')
-        print(np.sqrt(4*rho*Pstarstar*h0**2*(1+C_s*A0)/nu_max**2))
-        for i, l in enumerate(l_nl):
-            ax2.plot(k, VP_nonlocal(k, l)[0], color=okabe_ito[i], lw=1.2,label=r'$l_c = {:.0f}\,\mathrm{{km}}$'.format(l/1e3))
-            # ax1.plot(k, VP_nonlocal_viscous(k, l)[0], color=original_cmap(norm(l)), lw=1.2,label=r'$l_c = {:.0f}\,\mathrm{{km}}$'.format(l/1e3))
-            ax1.plot(k, VP_nonlocal_viscous(k, l)[0], color=okabe_ito[i], lw=1.2,label=r'$l_c = {:.0f}\,\mathrm{{km}}$'.format(l/1e3))
-
-        linestyles = ['-', '-.', '--']
-
-        count = 0
-        for omega, ls in zip(omega_EVP_div, linestyles):
-            if count == 0:
-                ax2.plot(k, omega, color='#4d4d1a', linestyle=ls, label = 'EVP')
-            else:
-                ax2.plot(k, omega, color='#4d4d1a', linestyle=ls)
-                
-            count+=1
-        for omega, ls in zip(omega_EVP_conv, linestyles):
-            ax3.plot(k, omega, color='#4d4d1a', linestyle=ls)
-
-        for ax in axs:
-            ax.grid(True, alpha=0.5, lw=0.5)
-            ax.tick_params(length=3, width=0.8)
-            
-        labels = ['(a)','(b)','(c)']
-        for i, ax in enumerate(axs):
-                ax.grid(alpha = 0.5)
-                ax.text(0.039, 0.955, labels[i], transform=ax.transAxes,
-                    fontsize=10,
-                    verticalalignment='top', horizontalalignment='left')
-
-        # fig.canvas.draw()  # force layout computation
-        ax_pos = ax2.get_position()
-        center = (ax_pos.x0 + ax_pos.x1) / 2
-        fig.supxlabel(r'$k$ (m$^{-1}$)', x=center-0.015)
-        # fig.supxlabel(r'$k$ (m$^{-1}$)',x = 0.465)
-        ax1.set_ylabel(r'$\text{Re}\{\omega_+\}$ (s$^{-1}$)')
-        ax2.set_ylabel(r'$\text{Re}\{\omega\}$ (s$^{-1}$)')
-        ax3.set_ylabel(r'$\text{Re}\{\omega\}$ (s$^{-1}$)')
-        # ax2.legend(fontsize=8, frameon=False)
-        ax2.set_xscale('log')
-        ax2.set_yscale('log')
-        ax1.set_xscale('log')
-        ax1.set_title('Viscous')
-        ax2.set_title('Plastic-Divergence')
-        ax3.set_title('Plastic-Convergence')
-        fig.align_ylabels()
-        # ax1.set_yscale('symlog', linthresh=1e-6)
-        # ax1.yaxis.set_minor_locator(SymmetricalLogLocator(
-        #     base=10, linthresh=1e-6, subs=np.arange(1, 10)
-        # ))
-        # ax1.set_ylim(-1e-2, 1e-6)
-        # ax1.yaxis.set_major_locator(FixedLocator(ticks_neg))
-        leg = ax2.legend(
-            loc='center left',
-            bbox_to_anchor=(1.01, 0.5),   # just outside ax1
-            frameon=False,
-            handlelength=0,
-            handletextpad=0
-        )
-        for handle, text in zip(leg.legend_handles, leg.get_texts()):
-            text.set_color(handle.get_color())
-        # ax1.yaxis.set_minor_formatter(mpl.ticker.NullFormatter())
-
-        # ax1.yaxis.set_major_formatter(mpl.ticker.FuncFormatter(every_two_symlog))
-
-
-        # ax2.yaxis.set_major_locator(mpl.ticker.LogLocator(base=10))
-        # ax2.yaxis.set_major_formatter(mpl.ticker.FuncFormatter(every_two))
-        fig.set_constrained_layout_pads(hspace=0.08)
-        fig.savefig("dispersion_VP_nonlocal.png", bbox_inches="tight")
-
-
-        plt.close(fig)
+        plot_paper(k, l_nl, 
+                    omega_vp_div, omega_sVP_viscous, 
+                    omega_EVP_viscous, omega_EVP_div, 
+                    omega_EVP_conv)
+    
+    elif args.mode == 'presentation':
+        plot_presentation(k, l_nl, 
+                    omega_vp_div, omega_sVP_viscous, 
+                    omega_EVP_viscous, omega_EVP_div, 
+                    omega_EVP_conv)
     return 0
 
 
