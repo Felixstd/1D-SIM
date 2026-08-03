df47cd1c10771ab1985537194c06c3a8841e433b
Felixstd <felix.st-denis@umontreal.ca>
2026-08-03 05:07:55 -0400

    seaiceparameters: new parameter
    new parameter, which is alpha^2, needed for the EVP computations.

--- StabilityAnalysis/seaiceparameters.py
+++ StabilityAnalysis/seaiceparameters.py
@@ -12,6 +12,7 @@ Pstarstar = 27.5e3*np.exp(-C_s*(1-A0))
 rhoi = 900
 T = 0.36*dt
 alpha = 1/((1+e**(-2))*T)
+alpha_e = (1+e**(-2))
 lamda_div =(np.sqrt(1+e**(-2))-1)/2
 lamda_conv = (-np.sqrt(1+e**(-2))-1)/2
 rho = 900
