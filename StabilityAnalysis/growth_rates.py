ac73d6a256afa1ccfaa994c30251377629c5f5f2
Felixstd <felix.st-denis@umontreal.ca>
2026-08-03 05:20:39 -0400

    growth_rates: added EVP and nEVP
    added a few functions to compute the dispersion relationship
    of the EVP family of models.

--- StabilityAnalysis/growth_rates.py
+++ StabilityAnalysis/growth_rates.py
@@ -83,7 +83,7 @@ def VP_local_viscous(k):
     # return omega, 4*rho*Pstarstar*h0**2*(1+C_s*A0)/(nu_max*k)**2
 
 
-def omega_EVP(lamda, k_tot):
+def omega_EVP_plastic(lamda, k_tot):
     k_tot = np.asarray(k_tot, dtype=np.float64)
     n = len(k_tot)
 
@@ -105,27 +105,48 @@ def omega_EVP(lamda, k_tot):
     omega_3 = roots[:, 2]
     return omega_1, omega_2, omega_3
 
-def EVP_viscous(k):
-    """
-    Function to plot the viscous VP
-    growth rates. It only outputs the positive growth rate
+def omega_EVP_viscous(k_tot) : 
+    
+    omega_1 = np.zeros(len(k_tot),dtype = complex)
+    omega_2 = np.zeros(len(k_tot),dtype = complex)
+    omega_3 = np.zeros(len(k_tot),dtype = complex)
+    
+    for i,k in enumerate(k_tot):
+        roots = np.roots([T*alpha_e*rhoi*h0,rho*h0,nu_max*k**(2),k**2*Pstarstar*(1+C_s*A0)/2])
+        omega_1[i],omega_2[i],omega_3[i] = roots.astype(complex)
+    
+    return [omega_1,omega_2,omega_3]
 
-    Args:
-        k (array): wavenumbers
+def omega_nEVP_plastic(k_tot, l = 10e3) : 
+    
 
-    Returns:
-        omega: positive root growth rate 
-    """
-    arg = 1 - 4*rho*Pstarstar*h0**2*(1+C_s*A0)*(alpha*T-1)/(nu_max*k)**2
-    omega = nu_max*k**2/(2*rho*h0*(alpha*T-1))*(-1 + np.sqrt(arg.astype(complex)))
+    l0 = l*A0
+    
+    omega_div  = np.zeros([3, len(k_tot)], dtype=complex)
+    omega_conv = np.zeros([3, len(k_tot)], dtype=complex)
+    
+    
+    for i,k in enumerate(k_tot):
+        omega_div[:,i] = np.roots([alpha_e*T,1,0,-lamda_div*k**2/(rhoi*h0*(k**2*l0**2+1))
+                              *(2*P0 + (C_s*A0-1)*Pstarstar*h0)])
+                              
+        omega_conv[:,i] = np.roots([alpha_e*T,1,0,-lamda_conv*k**2/(rhoi*h0*(k**2*l0**2+1))
+                              *(2*P0 + (C_s*A0-1)*Pstarstar*h0)])
 
-    x = 4 * rho * Pstarstar * h0**2 * (1 + C_s * A0) / (nu_max * k)**2
-    arg = 1 - x
+    
+    return omega_div, omega_conv
 
-    # Stable rewrite of nu_max*k**2/(2*rho*h0) * (-1 + sqrt(arg))
-    # using -1+sqrt(1-x) = -x/(1+sqrt(1-x)); the k**2 cancels analytically.
-    B = 2 * Pstarstar * h0 * (1 + C_s * A0) / nu_max
-    omega = -B / (1 + np.sqrt(arg.astype(complex)))
-
-    return omega, arg
+def omega_nEVP_viscous(k_tot, l = 10e3) : 
+    
+    l0 = l*A0
+    
+    omega_1 = np.zeros(len(k_tot),dtype = complex)
+    omega_2 = np.zeros(len(k_tot),dtype = complex)
+    omega_3 = np.zeros(len(k_tot),dtype = complex)
+    
+    for i,k in enumerate(k_tot):
+        roots = np.roots([T*alpha_e*rhoi*h0,rho*h0,nu_max*k**(2),k**2/(k**2*l0**2+1)*(2*P0-Pstarstar*h0*(1-C_s*A0))/2])
+        omega_1[i],omega_2[i],omega_3[i] = roots.astype(complex)
+    
+    return omega_1, omega_2, omega_3
     
